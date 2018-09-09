06d753f008f9fe976a2b10273576ead7463b8015
Pravin Gawale <PG00534975@techmahindra.com>
2018-09-09 22:44:28 +0530

    Initial Commit

---
+++ AnalogueTimeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnalogueTimeApp
+//
+//  Created by Pravin Gawale on 09/09/18.
+//  Copyright © 2018 Anita Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
